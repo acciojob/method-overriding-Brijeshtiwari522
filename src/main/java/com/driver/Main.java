c2c9257681b411616ed83fea678d4bd8b2c549b4
Brajesh <brijeshtiwari522@gmail.com>
2024-04-13 16:42:08 +0530

    Overriding Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
-  
+    class A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    class B extends A {
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
+    public static void main(String[] args) {
+        B objB =new B();
+        String resultAMethod = objB.meth() ;
+        System.out.println(resultAMethod);
+
+        String resultBMethod = objB.meth();
+        System.out.println(resultBMethod);
+    }
 }
